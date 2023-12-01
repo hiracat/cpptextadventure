f4d2f1d3747c089aaebdc4db6c432ea5620680d2
forest lehrman <forestgithub@skiff.com>
2023-12-01 12:39:52 -0500

    changed cmakelists.txt to put the executable in build/bin and added a typewriter effect to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
-
+#include <chrono>
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <thread>
 
-int main(){
-    std::cout << "hello world" << std::endl;
+void slowType(std::string toPrint) {
+    for (size_t i = 0; i < toPrint.size(); i++) {
+        std::cout << toPrint.at(i) << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+int main() {
+    slowType(std::string("hello world"));
 }
